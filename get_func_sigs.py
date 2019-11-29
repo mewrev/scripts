a8beb08b19552401a7ff956aaa00e51104035ae0
Robin Eklind <rnd0x00@gmail.com>
2019-11-29 02:06:00 +0100

    scripts: output function signatures as list instead of dictionary

--- get_func_sigs.py
+++ get_func_sigs.py
@@ -17,7 +17,11 @@ for seg in idautils.Segments():
 	for fn in idautils.Functions(seg, SegEnd(seg)):
 		func_addr = fn
 		func_name = idc.get_name(func_addr)
+		if func_name is None:
+			func_name = ""
 		func_type = idc.get_type(func_addr)
+		if func_type is None:
+			func_type = ""
 		func_sig = {"func_name": func_name, "func_type": func_type}
 		func_sigs[func_addr] = func_sig
 		func_addrs.append(func_addr)
@@ -27,26 +31,29 @@ func_addrs.sort()
 
 # Example output:
 #
-#    {
-#       "0x0": {
+#    [
+#       {
+#          "func_addr": "0x0",
 #          "func_name": "foo",
 #          "func_type": "int __cdecl(int x, int y)"
 #       },
-#       "0x20": {
+#       {
+#          "func_addr": "0x20",
 #          "func_name": "main",
 #          "func_type": "int __cdecl(int argc, const char **argv, const char **envp)"
 #       }
-#    }
-func_sigs_json = "{"
+#    ]
+func_sigs_json = "["
 for func_addr in func_addrs:
 	func_sig = func_sigs[func_addr]
 	func_name = func_sig["func_name"]
 	func_type = func_sig["func_type"]
-	func_sigs_json += "\n\t\"0x%08X\": {"%(func_addr)
+	func_sigs_json += "\n\t{"
+	func_sigs_json += "\n\t\t\"func_addr\": \"0x%08X\","%(func_addr)
 	func_sigs_json += "\n\t\t\"func_name\": \"%s\","%(func_name)
 	func_sigs_json += "\n\t\t\"func_type\": \"%s\""%(func_type)
 	func_sigs_json += "\n\t},"
 func_sigs_json = func_sigs_json.rstrip(',')
-func_sigs_json += "\n}"
+func_sigs_json += "\n]"
 
 print(func_sigs_json)
