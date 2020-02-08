94da0a673e4cdfed6a1a4476374c74511b576375
Robin Eklind <rnd0x00@gmail.com>
2020-02-08 18:46:05 +0100

    scripts: update format of function chunks JSON output
    After this commit, the format is as follows. Map from function address
    to list of function chunks (of which the function is composed). Each
    chunk is denoted by its start address and size in bytes.
    
    Example with two function at address 0x0 and 0xB. Note that the function
    at address 0x0 is discontinuous, as the chunk 0xB exist in between the
    two chunks 0x0 and 0x11.
    
    	{
    		"0x00000000": [
    			{
    				"chunk_addr": "0x00000000",
    				"chunk_size": 11
    			},
    			{
    				"chunk_addr": "0x00000011",
    				"chunk_size": 6
    			}
    		],
    		"0x0000000B": [
    			{
    				"chunk_addr": "0x0000000B",
    				"chunk_size": 6
    			}
    		]
    	}
    
    Updates mewmew/lift#11.

--- get_chunk_addrs.py
+++ get_chunk_addrs.py
@@ -5,76 +5,68 @@ import idc
 #
 # based on: https://reverseengineering.stackexchange.com/a/14726
 
-# chunks maps from chunk start address to a record containing the chunk size in
-# bytes and a list of parent functions containing the chunk (as a chunk may be
-# used by more than one function).
-chunks = {}
-chunk_start_addrs = []
+# func_chunks maps from function address to list of chunks (each with a start
+# address and size in bytes).
+func_chunks = {}
+func_addrs = []
 for seg in idautils.Segments():
 	# Skip extern segment; as used by IDA for external functions.
 	if idc.get_segm_attr(seg, SEGATTR_TYPE) == idc.SEG_XTRN:
 		#print("skipping segment ", idc.get_segm_name(seg))
 		continue
 	for fn in idautils.Functions(seg, idc.get_segm_end(seg)):
-		fn_addr = fn
+		func_addr = fn
 		#func_name = idc.get_name(fn)
+		# chunks is a list of chunks contained within the current function.
+		chunks = []
 		for (chunk_start, chunk_end) in idautils.Chunks(fn):
-			if not chunk_start in chunks:
-				chunk_size = chunk_end - chunk_start
-				chunks[chunk_start] = {"chunk_size": chunk_size, "parent_funcs": []}
-			chunks[chunk_start]["parent_funcs"].append(fn_addr)
-				#chunk = chunks[chunk_start]
-				#chunk["parent_funcs"].append(fn_addr)
-				#chunks[chunk_start] = chunk # TODO: check if this line is needed.
-			chunk_start_addrs.append(chunk_start)
+			chunk_addr = chunk_start
+			chunk_size = chunk_end - chunk_start
+			chunk = {"chunk_addr": chunk_addr, "chunk_size": chunk_size}
+			chunks.append(chunk)
+		# Sort chunks based on start address.
+		chunks = sorted(chunks, key=lambda chunk: chunk['chunk_addr']) # sort by chunk address
+		func_chunks[func_addr] = chunks
+		func_addrs.append(func_addr)
 
-# Sort chunk starting addresses to be used as key.
-chunk_start_addrs.sort()
-
-# Sort parent funcs.
-for chunk_start_addr in chunk_start_addrs:
-	chunk = chunks[chunk_start_addr]
-	chunk["parent_funcs"].sort()
+# Sort function addresses to be used as key.
+func_addrs.sort()
 
 # Example output:
 #
-#    [
-#       {
-#          "chunk_addr": "0xDAF0",
-#          "chunk_size": 331,
-#          "parent_funcs": [
-#             "0xDC70"
-#          ]
-#       },
-#       {
-#          "chunk_addr": "0xDC40",
-#          "chunk_size": 8,
-#          "parent_funcs": [
-#             "0xDC70",
-#          ]
-#       },
-#       {
-#          "chunk_addr": "0xDC50",
-#          "chunk_size": 19,
-#          "parent_funcs": [
-#             "0xDC70"
-#          ]
-#       }
-#    ]
-chunks_json = "["
-for chunk_start_addr in chunk_start_addrs:
-	chunk = chunks[chunk_start_addr]
-	chunk_size = chunk["chunk_size"]
-	chunks_json += "\n\t{"
-	chunks_json += "\n\t\t\"chunk_addr\": \"0x%08X\","%(chunk_start_addr)
-	chunks_json += "\n\t\t\"chunk_size\": %d,"%(chunk_size)
-	chunks_json += "\n\t\t\"parent_funcs\": ["
-	for parent_func in chunk["parent_funcs"]:
-		chunks_json += "\n\t\t\t\"0x%08X\","%(parent_func)
+#    {
+#       "0x00000000": [
+#          {
+#             "chunk_addr": "0x00000000",
+#             "chunk_size": 11
+#          },
+#          {
+#             "chunk_addr": "0x00000011",
+#             "chunk_size": 6,
+#          }
+#       ],
+#       "0x0000000B": [
+#          {
+#             "chunk_addr": "0x0000000B",
+#             "chunk_size": 6
+#          }
+#       ]
+#    }
+
+chunks_json = "{"
+for func_addr in func_addrs:
+	chunks_json += "\n\t\"0x%08X\": ["%(func_addr)
+	chunks = func_chunks[func_addr]
+	for chunk in chunks:
+		chunk_addr = chunk["chunk_addr"]
+		chunk_size = chunk["chunk_size"]
+		chunks_json += "\n\t\t{"
+		chunks_json += "\n\t\t\t\"chunk_addr\": \"0x%08X\","%(chunk_addr)
+		chunks_json += "\n\t\t\t\"chunk_size\": %d"%(chunk_size)
+		chunks_json += "\n\t\t},"
 	chunks_json = chunks_json.rstrip(',')
-	chunks_json += "\n\t\t]"
-	chunks_json += "\n\t},"
+	chunks_json += "\n\t],"
 chunks_json = chunks_json.rstrip(',')
-chunks_json += "\n]"
+chunks_json += "\n}"
 
 print(chunks_json)
